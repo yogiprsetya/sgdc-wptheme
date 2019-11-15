44a45dc474cfd6770e917634d85c75840796c700
Yogi Prasetya <yogiprsetya@gmail.com>
2019-11-15 16:37:08 +0700

    pages

--- sgdc/page.php
+++ sgdc/page.php
@@ -15,8 +15,8 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<section class="site-content row container">
+		<main role="main">
 
 		<?php
 		while ( have_posts() ) :
@@ -25,16 +25,16 @@ get_header();
 			get_template_part( 'template-parts/content', 'page' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			// if ( comments_open() || get_comments_number() ) :
+			// 	comments_template();
+			// endif;
 
 		endwhile; // End of the loop.
 		?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</main>
+		<?php get_sidebar(); ?>
+	</section>
 
 <?php
-get_sidebar();
 get_footer();
--- sgdc/template-parts/content-page.php
+++ sgdc/template-parts/content-page.php
@@ -10,11 +10,9 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-
 	<?php sgdc_post_thumbnail(); ?>
+
+	<?php the_title( '<h1 itemprop="headline">', '</h1>' ); ?>
 
 	<div class="entry-content">
 		<?php
@@ -27,26 +25,5 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'sgdc' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					get_the_title()
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
-</article><!-- #post-<?php the_ID(); ?> -->
+
+</article>
